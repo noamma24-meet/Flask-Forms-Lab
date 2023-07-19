583ce059f4c8c4c7c04f6ba49bb767b7d3a3c6c0
noamma24-meet <139681176+noamma24-meet@users.noreply.github.com>
2023-07-19 14:03:40 +0300

    4 should've been a bonus tbh

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -8,19 +8,35 @@ app = Flask(  # Create a flask app
 )
 
 
-username = "llo2ay"
+username = "noam"
 password = "123"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+facebook_friends=["ava","hila","george","yuka"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/', methods=['GET','POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
-  
+	if request.method =='POST':
+		user=request.form["username"]
+		passy=request.form["password"]
+		if user==username and passy==password:
+			return redirect(url_for('home'))
+	return render_template('login.html')
+
+@app.route('/home')
+def home():
+	return render_template("home.html", friends=facebook_friends)
+
+@app.route('/friend_exists/<string:name>', methods=['GET','POST'])
+def friends(name):
+	if name in facebook_friends:
+		return render_template("/friend_exists.html", status="True")
+	else:
+				return render_template("/friend_exists.html", status="False")
 
 
 
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
-    debug=True
+		debug=True
 	)
+
